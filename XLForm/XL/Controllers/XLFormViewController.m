b89f4217c059c0346f4ca1c2cb92ce48a9a676be
wangchi <wangchi@meican.com>
2016-10-08 10:11:54 +0800

    test

--- XLForm/XL/Controllers/XLFormViewController.m
+++ XLForm/XL/Controllers/XLFormViewController.m
@@ -22,7 +22,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
+//haha my version
 #import "UIView+XLFormAdditions.h"
 #import "NSObject+XLFormAdditions.h"
 #import "XLFormViewController.h"
